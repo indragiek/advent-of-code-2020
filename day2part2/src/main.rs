3efef3a30e69eb3c46a2512595771fee827da454
Indragie Karunaratne <indragie@specto.dev>
2020-12-04 22:08:52 -0800

    Day 2 part 2

--- day2part2/src/main.rs
+++ day2part2/src/main.rs
@@ -7,7 +7,7 @@ use std::process;
 #[derive(Debug)]
 enum InputError {
     InvalidLineFormat,
-    InvalidMinMax,
+    InvalidPositions,
     InvalidLetter,
 }
 
@@ -15,7 +15,7 @@ impl fmt::Display for InputError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             InputError::InvalidLineFormat => write!(f, "invalid line format"),
-            InputError::InvalidMinMax => write!(f, "invalid min or max"),
+            InputError::InvalidPositions => write!(f, "invalid positions"),
             InputError::InvalidLetter => write!(f, "invalid letter"),
         }
     }
@@ -26,7 +26,7 @@ impl std::error::Error for InputError {}
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        println!("usage: day2part1 <path to input text file>");
+        println!("usage: day2part2 <path to input text file>");
         process::exit(1);
     }
     let filename = &args[1];
@@ -41,16 +41,26 @@ fn main() {
 }
 
 struct PasswordPolicy {
-    min: usize,
-    max: usize,
+    pos_1: usize,
+    pos_2: usize,
     letter: char,
     password: String,
 }
 
 impl PasswordPolicy {
     fn is_valid(&self) -> bool {
-        let count = self.password.chars().filter(|&c| c == self.letter).count();
-        return (count >= self.min) && (count <= self.max);
+        match (
+            self.password.chars().nth(self.pos_1 - 1),
+            self.password.chars().nth(self.pos_2 - 1),
+        ) {
+            (Some(c), None) => return c == self.letter,
+            (None, Some(c)) => return c == self.letter,
+            (Some(c1), Some(c2)) => {
+                return (c1 == self.letter && c2 != self.letter)
+                    || (c1 != self.letter && c2 == self.letter)
+            }
+            (None, None) => return false,
+        }
     }
 }
 
@@ -61,15 +71,15 @@ fn parse_line(line: String) -> Result<PasswordPolicy, InputError> {
     }
     let min_max_components: Vec<&str> = components[0].split("-").collect();
     if min_max_components.len() < 2 {
-        return Err(InputError::InvalidMinMax);
+        return Err(InputError::InvalidPositions);
     }
 
-    let min = min_max_components[0]
+    let pos_1 = min_max_components[0]
         .parse::<usize>()
-        .map_err(|_| InputError::InvalidMinMax)?;
-    let max = min_max_components[1]
+        .map_err(|_| InputError::InvalidPositions)?;
+    let pos_2 = min_max_components[1]
         .parse::<usize>()
-        .map_err(|_| InputError::InvalidMinMax)?;
+        .map_err(|_| InputError::InvalidPositions)?;
 
     let letter_str = components[1];
     if letter_str.is_empty() {
@@ -78,8 +88,8 @@ fn parse_line(line: String) -> Result<PasswordPolicy, InputError> {
     let letter = letter_str.chars().nth(0).ok_or(InputError::InvalidLetter)?;
 
     Ok(PasswordPolicy {
-        min: min,
-        max: max,
+        pos_1: pos_1,
+        pos_2: pos_2,
         letter: letter,
         password: String::from(components[2]),
     })
