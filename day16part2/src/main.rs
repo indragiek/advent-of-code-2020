8b81cb9c52847a16ab0d0d3b6ba550dc069b651c
Indragie Karunaratne <indragie@specto.dev>
2020-12-16 20:55:19 -0700

    Day 16 part 2

--- day16part2/src/main.rs
+++ day16part2/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -36,11 +37,64 @@ fn main() {
                 ParseStage::NearbyTickets => nearby_tickets.push(parse_ticket(l)),
             },
         });
-    let rate: u32 = nearby_tickets
+    let num_fields = your_ticket.len();
+    let valid_tickets: Vec<Vec<u32>> = nearby_tickets
+        .into_iter()
+        .filter(|ticket| is_valid_ticket(ticket, &rules))
+        .collect();
+    let mut valid_rules: Vec<(usize, Vec<String>)> = (0..num_fields)
+        .map(|i| -> (usize, Vec<u32>) {
+            (i, valid_tickets.iter().map(|ticket| ticket[i]).collect())
+        })
+        .map(|(i, values)| -> (usize, Vec<String>) {
+            (
+                i,
+                rules
+                    .iter()
+                    .filter(|rule| {
+                        values
+                            .iter()
+                            .map(|&value| !rule.validate(value))
+                            .filter(|&x| x)
+                            .count()
+                            == 0
+                    })
+                    .map(|rule| rule.field.clone())
+                    .collect(),
+            )
+        })
+        .collect();
+    valid_rules.sort_by(|a, b| a.1.len().partial_cmp(&b.1.len()).unwrap());
+
+    let mut claimed_fields = HashSet::new();
+    let field_assignments: Vec<(usize, String)> = valid_rules
         .iter()
-        .map(|ticket| ticket_scanning_error_rate(ticket, &rules))
-        .sum();
-    println!("{}", rate);
+        .map(|(i, fields)| -> (usize, String) {
+            let available_fields: Vec<String> = fields
+                .clone()
+                .into_iter()
+                .filter(|field| !claimed_fields.contains(field))
+                .collect();
+            (
+                i.clone(),
+                match available_fields.len() {
+                    1 => {
+                        let field = available_fields[0].clone();
+                        claimed_fields.insert(field.clone());
+                        field
+                    }
+                    _ => "<undefined>".to_string(),
+                },
+            )
+        })
+        .collect();
+
+    let product: u64 = field_assignments
+        .iter()
+        .filter(|(_, field)| field.starts_with("departure"))
+        .map(|&(i, _)| your_ticket[i] as u64)
+        .product();
+    println!("{}", product);
 }
 
 struct Range {
@@ -88,21 +142,16 @@ fn parse_rule(line: &str) -> Option<Rule> {
     });
 }
 
-fn ticket_scanning_error_rate(ticket: &Vec<u32>, rules: &Vec<Rule>) -> u32 {
+fn is_valid_ticket(ticket: &Vec<u32>, rules: &Vec<Rule>) -> bool {
     return ticket
         .iter()
         .map(|&field| {
-            if rules
+            rules
                 .iter()
                 .map(|rule| rule.validate(field))
                 .filter(|&x| x)
                 .count()
-                == 0
-            {
-                return field;
-            } else {
-                return 0;
-            }
         })
-        .sum();
+        .find(|&count| count == 0)
+        .is_none();
 }
